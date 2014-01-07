d42aeb449b8ef006fc90327dee167f2115d606df
William Oliveira <sudowilliam@gmail.com>
2014-01-07 03:37:16 -0200

    adds fake data

--- assets/app/modules/angularMail.js
+++ assets/app/modules/angularMail.js
@@ -14,3 +14,39 @@ function routeConfig($routeProvider) {
 }
 
 AngularMailServices.config(routeConfig);
+
+emails = [
+  {
+    id: 0,
+    sender: 'sudowilliam@gmail.com',
+    subject: 'Testing email app',
+    date: 'Jan 7, 2014 3:34',
+    recipients: ['email@email.com'],
+    message: 'Hey, how you doing!'
+  },
+  {
+    id: 1,
+    sender: 'lea@gmail.com',
+    subject: 'Hello email app',
+    date: 'Jan 7, 2014 3:36',
+    recipients: ['email@domain.com'],
+    message: 'Heeeeeyeyey, how you doing!'
+  },
+  {
+    id: 2,
+    sender: 'vader@gmail.com',
+    subject: 'Father',
+    date: 'Jan 7, 2014 3:34',
+    recipients: ['email@email.com'],
+    message: 'Luke, Im your father!'
+  },
+  {
+    id: 3,
+    sender: 'chewie@gmail.com',
+    subject: 'hnnnhrrhhh!',
+    date: 'Jan 7, 2014 3:34',
+    recipients: ['email@starwars.com'],
+    message: 'hnnnhrrhhh awwgggghhh wrrhwrwwhw!'
+  },
+]
+
